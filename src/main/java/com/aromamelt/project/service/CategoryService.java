f7b2b1930900239669606fcdf64ebb48a303263e
Sanskar <sanskar.shubham28@gmail.com>
2026-03-09 00:07:36 +0530

    DTO updated

--- src/main/java/com/aromamelt/project/service/CategoryService.java
+++ src/main/java/com/aromamelt/project/service/CategoryService.java
@@ -1,12 +1,13 @@
 package com.aromamelt.project.service;
 
 import com.aromamelt.project.model.Category;
+import com.aromamelt.project.payload.CategoryDTO;
+import com.aromamelt.project.payload.CategoryResponse;
 
-import java.util.List;
 
 public interface CategoryService {
-    List<Category> getAllCategories();
-    void createCategory(Category category);
-    String deleteCategory(Long categoryId);
-    Category updateCategory(Category category, Long categoryId);
+    CategoryResponse getAllCategories();
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
+    CategoryDTO deleteCategory(Long categoryId);
+    CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
 }
